8be9ebccb25057ee3abfd1ff80582628740c04e6
TomekStolarz <tomekstolarz0@gmail.com>
2023-03-27 15:19:58 +0200

    -[feat] [BE] added mongo connection

--- tablook-api/src/app.module.ts
+++ tablook-api/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PhoneCodeModule } from './phone-code/phone-code.module';
 
 @Module({
-  imports: [PhoneCodeModule],
+  imports: [
+    PhoneCodeModule,
+    MongooseModule.forRoot('mongodb://localhost/tablook'),
+  ],
   controllers: [],
   providers: [],
 })
