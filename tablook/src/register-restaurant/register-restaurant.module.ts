c946499542caeb32c551f16d5dd904d87fede811
TomekStolarz <tomekstolarz0@gmail.com>
2023-04-18 17:42:41 +0200

    -[refactor] alingment forms

--- tablook/src/register-restaurant/register-restaurant.module.ts
+++ tablook/src/register-restaurant/register-restaurant.module.ts
@@ -5,10 +5,12 @@ import { SharedModule } from 'src/shared/shared.module';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { RegisterRestaurantRoutingModule } from './register-restaurant-routing.module';
 
 @NgModule({
 	declarations: [RegisterRestaurantComponent],
 	imports: [
+		RegisterRestaurantRoutingModule,
 		CommonModule,
 		MatInputModule,
 		MatButtonModule,
