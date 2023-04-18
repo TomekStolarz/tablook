d3b334c7efde7ca5388b554bd5f5f7a8ea016816
TomekStolarz <tomekstolarz0@gmail.com>
2023-04-18 20:43:39 +0200

    -[fix] added aditional resources to phone codes

--- tablook-api/src/phone-code/phone-code.service.ts
+++ tablook-api/src/phone-code/phone-code.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { AxiosError } from 'axios';
-import { catchError, map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, switchMap, tap } from 'rxjs';
 import { CountryPhoneCode } from './models/country-phone-code.interface';
 import { PhoneCodeResponse } from './models/phone-code-response.interface';
 
@@ -25,8 +25,36 @@ export class PhoneCodeService {
         }),
         tap(() => this.logger.log('Phone codes data fetched successfully')),
         catchError((error: AxiosError) => {
-          this.logger.error(error.response.data);
-          throw 'Cannot get data from remote resource';
+          this.logger.error(
+            error.response?.data || 'Cannot get data from remote resource',
+          );
+          return this.getPhoneCodesBackup();
+        }),
+      );
+  }
+
+  private getPhoneCodesBackup(): Observable<CountryPhoneCode[]> {
+    return this.httpService
+      .get<{ [key: string]: string }>('http://country.io/phone.json')
+      .pipe(
+        tap(() => this.logger.log('Get data from additional resources')),
+        map((codes) => {
+          return Object.entries(codes.data).map((data) => {
+            return {
+              countryKey: data[0],
+              phoneCode: data[1],
+            };
+          });
+        }),
+        catchError((error: AxiosError) => {
+          this.logger.error(
+            error.response?.data ||
+              'Cannot get data from additional remote resource',
+          );
+          throw new HttpException(
+            'Cannot get data from remote resource',
+            HttpStatus.BAD_REQUEST,
+          );
         }),
       );
   }
