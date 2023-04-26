5cb65dd3c1b97869dc27b8628fe0e7145e0881cc
TomekStolarz <tomekstolarz0@gmail.com>
2023-04-26 11:59:48 +0200

    -[[fix][BE] user fields correction for restaurant details

---
+++ tablook-api/src/user/models/opening-hour.interface.ts
@@ -0,0 +1,6 @@
+import { Day } from './day.enum';
+
+export interface OpeningHour {
+  day: Day;
+  hours: string[];
+}
--- tablook-api/src/user/models/user-details.interface.ts
+++ tablook-api/src/user/models/user-details.interface.ts
@@ -1,5 +1,14 @@
 import { Address } from './address.interface';
+import { OpeningHour } from './opening-hour.interface';
+import { Table } from './table.interface';
 
 export class UserDetails {
-  address?: Address;
+  address: Address;
+  openingHours: OpeningHour[];
+  images: string[];
+  description: string;
+  googleMapsLink: string;
+  tags: string[];
+  tables: Table[];
+  tablesMap: string;
 }
