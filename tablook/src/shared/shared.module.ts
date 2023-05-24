581a50db1c4e40a3eb1edd40cf2299a4f660b34c
TomekStolarz <tomekstolarz0@gmail.com>
2023-05-24 21:13:51 +0200

    -[feat] added select time range dialog

--- tablook/src/shared/shared.module.ts
+++ tablook/src/shared/shared.module.ts
@@ -1,22 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ErrorNotificationComponent } from './components/error-notification/error-notification.component';
 import { TrimLengthPipe } from './pipes/trim-length.pipe';
 import { CustomSnackbarComponent } from './components/custom-snackbar/custom-snackbar.component';
 import { CustomSnackbarService } from './services/custom-snackbar.service';
 import { UpperFirstPipe } from './pipes/upper-first.pipe';
+import { PhonePrefixPipe } from './pipes/phone-prefix.pipe';
+import { SelectTimeComponent } from './components/select-time/select-time.component';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMatTimepickerModule } from 'ngx-mat-timepicker';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-	imports: [MatSnackBarModule, MatCardModule, CommonModule],
+	imports: [
+		MatSnackBarModule,
+		MatCardModule,
+		CommonModule,
+		MatDialogModule,
+		MatButtonModule,
+		MatInputModule,
+		ReactiveFormsModule,
+		NgxMatTimepickerModule,
+	],
 	declarations: [
 		ErrorNotificationComponent,
 		TrimLengthPipe,
 		CustomSnackbarComponent,
-  UpperFirstPipe,
+		UpperFirstPipe,
+		PhonePrefixPipe,
+		SelectTimeComponent,
 	],
 	providers: [CustomSnackbarService],
-	exports: [ErrorNotificationComponent],
+	exports: [ErrorNotificationComponent, PhonePrefixPipe, SelectTimeComponent],
 })
 export class SharedModule {}
