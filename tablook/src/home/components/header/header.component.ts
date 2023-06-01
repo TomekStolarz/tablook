8ba36d00cff81493a7a4fae67408604b57dea5e9
TomekStolarz <tomekstolarz0@gmail.com>
2023-06-01 19:12:33 +0200

    -[feat] added responsive view to header

--- tablook/src/home/components/header/header.component.ts
+++ tablook/src/home/components/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, Input, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 	@Input()
 	username?: string;
+	isMobile = false;
 
-	constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService, private responsive: BreakpointObserver) {}
+
+	ngOnInit(): void {
+		this.responsive.observe([Breakpoints.TabletPortrait, Breakpoints.HandsetPortrait])
+			.subscribe((result) => {
+				if (result.matches) {
+					this.isMobile = true;
+				} else {
+					this.isMobile = false;
+				}
+			});
+	}
 
 	logout(): void {
 		this.authService.logout();
