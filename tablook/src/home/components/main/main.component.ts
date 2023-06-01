92e24aad8cad1e55221ddede7112c50b99581ccd
TomekStolarz <tomekstolarz0@gmail.com>
2023-06-01 19:07:11 +0200

    -[feat] added responsive to main component

--- tablook/src/home/components/main/main.component.ts
+++ tablook/src/home/components/main/main.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-main',
 	templateUrl: './main.component.html',
 })
-export class MainComponent {
-	constructor(private router: Router) {}
+export class MainComponent implements OnInit{
+	constructor(private router: Router,
+		private responsive: BreakpointObserver) { }
+	
+	isMobile = false;
+	
+	ngOnInit(): void {
+		this.responsive.observe([Breakpoints.TabletPortrait, Breakpoints.HandsetPortrait])
+			.subscribe((result) => {
+				if (result.matches) {
+					this.isMobile = true;
+				} else {
+					this.isMobile = false;
+				}
+			});
+	}
 
 	goToSearch() {
 		this.router.navigate(['/home/search']);
