1fff38be0d43b9d8d742cfa58c7cbcf7b507be68
TomekStolarz <tomekstolarz0@gmail.com>
2023-08-31 20:08:41 +0200

    -[feat] refresh data about tables

--- tablook/src/app/interfaces/user-info.interface.ts
+++ tablook/src/app/interfaces/user-info.interface.ts
@@ -2,7 +2,7 @@ import { UserType } from 'src/app/interfaces/user-type.enum';
 import { UserDetails } from 'src/register-module/interfaces/user-details.interface';
 
 export interface UserInfo {
-	id?: string;
+	id: string;
 	name?: string;
 	email?: string;
 	type?: UserType;
