620e2bcd36f492dc0ea555dd3636d6a47991e0f5
TomekStolarz <tomekstolarz0@gmail.com>
2023-09-12 21:58:16 +0200

    -[refactor] removed unclosed subscription

--- tablook/src/home/components/home/home.component.ts
+++ tablook/src/home/components/home/home.component.ts
@@ -1,21 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { UserInfo } from 'src/app/interfaces/user-info.interface';
-import { AuthService } from 'src/app/services/auth.service';
 import { selectUser } from 'src/app/store/user.selector';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
-	user?: UserInfo;
+export class HomeComponent  {
+	private readonly store = inject(Store);
+	
 	user$ = this.store.select(selectUser);
-
-	constructor(private store: Store, private authService: AuthService) {}
-
-	ngOnInit(): void {
-		this.user$.subscribe((user) => (this.user = user));
-		this.authService.isAuth().subscribe();
-	}
+	
 }
