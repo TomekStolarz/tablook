179fc7c20d7424c27b30849cc56ae7dbbf1cdbcd
TomekStolarz <tomekstolarz0@gmail.com>
2023-09-13 21:26:50 +0200

    -[feat] displaying favourite item list

--- tablook/src/account-module/components/favourites/favourites.component.ts
+++ tablook/src/account-module/components/favourites/favourites.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable, forkJoin, map, switchMap } from 'rxjs';
+import { RestaurantFavouriteTile } from 'src/account-module/models/restaurant-favourite-tile';
+import { selectUser } from 'src/app/store/user.selector';
+import { RestaurantDetailsService } from 'src/home/restaurant-details/services/restaurant-details.service';
+import { FavouriteService } from 'src/shared/services/favourite.service';
 
 @Component({
   selector: 'app-favourites',
@@ -6,5 +12,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./favourites.component.scss']
 })
 export class FavouritesComponent {
+  private readonly favService = inject(FavouriteService);
 
+  private readonly store = inject(Store);
+
+  protected favouriteData$: Observable<RestaurantFavouriteTile[]> = this.store.pipe(
+    select(selectUser),
+    map((userInfo) => userInfo?.id || ''),
+    switchMap((userId) => this.favService.getFavourites(userId)),
+  ); 
 }
