8f35e402e04c5fb10e93d3c1d7ca46a9b847d563
TomekStolarz <tomekstolarz0@gmail.com>
2023-10-16 23:57:45 +0200

    -[fix] fixed broken date formatting

--- tablook/src/shared/pipes/date-to-time.pipe.ts
+++ tablook/src/shared/pipes/date-to-time.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DateToTimePipe implements PipeTransform {
 
   transform(date: Date): string {
+    if (typeof date !== 'string') {
+      return `${date}`;
+    }
     const properDate = new Date(date);
     const minutes = properDate.getMinutes();
     return `${properDate.getHours()}:${minutes < 10 ? `0${minutes}`: minutes}`;
