d96008f7870927296707b79e7deaf0bea90ff63d
TomekStolarz <tomekstolarz0@gmail.com>
2023-11-17 13:47:05 +0100

    -[feat] ajoi lang formatter

--- tablook-api/src/app.module.ts
+++ tablook-api/src/app.module.ts
@@ -44,13 +44,76 @@ import { SafeString, escapeExpression } from 'handlebars';
       template: {
         dir: join(__dirname, '../mail/templates'),
         adapter: new HandlebarsAdapter({
-          link: (text, url) => {
+          link: (text, url, style?) => {
             const propurl = escapeExpression(url),
               proptext = escapeExpression(text);
 
             return new SafeString(
-              `<a href="${propurl}" target="_blank" class="btn btn-dark">${proptext}</a>`,
+              `<a style="${style}" href="${propurl}" target="_blank" class="btn btn-dark">${proptext}</a>`,
             );
+          },
+          styleResource: (url?) => {
+            return new SafeString(
+              `<link
+              href="${url}"
+              rel="stylesheet"
+            />`,
+            );
+          },
+          basicHeadResources: () => {
+            return new SafeString(
+              `
+              <meta charset="utf-8" />
+              <title>Tablook</title>
+              <base href="/" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <link rel="icon" type="image/x-icon" href="favicon.ico" />
+              <link
+                href="https://fonts.googleapis.com/css?family=Space Mono"
+                rel="stylesheet"
+              />
+              <link rel="preconnect" href="https://fonts.gstatic.com" />
+              <link
+                href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap"
+                rel="stylesheet"
+              />
+              <link
+                href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+                rel="stylesheet"
+              />
+                   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+          
+          
+              `,
+            );
+          },
+          basicStyle: () => {
+            return new SafeString(
+              `
+              <style>
+              * {
+                font-family: "Trebuchet MS";
+                font-style: normal;
+              }
+
+              .holder, footer {
+                  display: grid;
+              }
+
+              .holder > * {
+                text-align: center;
+              }
+
+              footer {
+                  color: white;
+                  background: black;
+              }
+              </style>
+              `,
+            );
+          },
+          trustedHtml: (htmlCode) => {
+            return new SafeString(htmlCode);
           },
         }),
       },
--- tablook-api/src/mail/mail/notification.service.ts
+++ tablook-api/src/mail/mail/notification.service.ts
@@ -38,6 +38,9 @@ export class NotificationService {
         confirmationStatus: confirmationStatus === ConfirmationStatus.CONFIRMED,
         name: user.name[0].toUpperCase() + user.name.slice(1),
         date: date.toDateString(),
+        icon: this.getConfirmationIcon(
+          confirmationStatus === ConfirmationStatus.CONFIRMED,
+        ),
       },
     });
   }
@@ -53,6 +56,10 @@ export class NotificationService {
         name: restaurant.name,
         panelLink: `${config.front}/home/restaurant-account/reservations`,
         panel: 'panel',
+        style: `padding: 5px;
+        border-radius: 10px;
+        background-color: #0e8d7e;
+        color: white;`,
       },
     });
   }
@@ -90,5 +97,11 @@ export class NotificationService {
         order: order,
       },
     });
+  }
+
+  getConfirmationIcon(confirmatiom: boolean) {
+    return confirmatiom
+      ? `<svg xmlns="http://www.w3.org/2000/svg" fill="#00b337" height="24" viewBox="0 -960 960 960" width="24"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
+      : `<svg xmlns="http://www.w3.org/2000/svg" fill="rgba(255, 51, 51, 0.88888)" height="24" viewBox="0 -960 960 960" width="24"><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`;
   }
 }
